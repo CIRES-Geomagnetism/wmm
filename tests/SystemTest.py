d5d25663cdd90af64eacc2973c025b1b144b3a98
Li-Yin Young <liyo6397@colorado.edu>
2024-11-20 09:27:44 -0700

    Update the coefficient files and System Test based on WMM2025

--- tests/SystemTest.py
+++ tests/SystemTest.py
@@ -1,58 +1,79 @@
+import os
+import shutil
 from math import fabs
 from wmm.build import wmm_calc
 
-def check_base_results(res_map, lat, lon, alt, dyear, dec, inc, h, x, y, z, f, tol, res_fp):
 
-    if (fabs(round(res_map["x"], 1) - x) > tol):
-        res_fp.write(f"x,{lat},{lon},{alt},{dyear},{res_map['x']},{x}\n")
+def write_diff_results(file_path, inputs, true_v, pred_v):
 
-    if (fabs(round(res_map["y"], 1) - y) > tol):
-        res_fp.write(f"y,{lat},{lon},{alt},{dyear},{res_map['y']},{y}\n")
-    if (fabs(round(res_map["z"], 1) - z) > tol):
-        res_fp.write(f"z,{lat},{lon},{alt},{dyear},{res_map['z']},{z}\n")
-    if (fabs(round(res_map["h"], 1) - h) > tol):
-        res_fp.write(f"h,{lat},{lon},{alt},{dyear},{res_map['h']},{h}\n")
-    if (fabs(round(res_map["f"], 1) - f) > tol):
-        res_fp.write(f"f,{lat},{lon},{alt},{dyear},{res_map['f']},{f}\n")
-    if (fabs(round(res_map["dec"], 2) - dec) > tol):
-        res_fp.write(f"dec,{lat},{lon},{alt},{dyear},{res_map['dec']},{dec}\n")
-    if (fabs(round(res_map["inc"], 2) - inc) > tol):
-        res_fp.write(f"inc,{lat},{lon},{alt},{dyear},{res_map['inc']},{inc}\n")
-
-def check_sv_results(res_map, lat, lon, alt, dyear, dec, inc, h, x, y, z, f, tol, res_fp):
-
-    if (fabs(round(res_map["dx"], 1) - x) > tol):
-        res_fp.write(f"x,{lat},{lon},{alt},{dyear},{res_map['dx']},{x}\n")
-
-    if (fabs(round(res_map["dy"], 1) - y) > tol):
-        res_fp.write(f"y,{lat},{lon},{alt},{dyear},{res_map['dy']},{y}\n")
-    if (fabs(round(res_map["dz"], 1) - z) > tol):
-        res_fp.write(f"z,{lat},{lon},{alt},{dyear},{res_map['dz']},{z}\n")
-    if (fabs(round(res_map["dh"], 1) - h) > tol):
-        res_fp.write(f"h,{lat},{lon},{alt},{dyear},{res_map['dh']},{h}\n")
-    if (fabs(round(res_map["df"], 1) - f) > tol):
-        res_fp.write(f"f,{lat},{lon},{alt},{dyear},{res_map['df']},{f}\n")
-    if (fabs(round(res_map["ddec"]/60, 1) - dec) > tol):
-        res_fp.write(f"dec,{lat},{lon},{alt},{dyear},{res_map['ddec']},{dec}\n")
-    if (fabs(round(res_map["dinc"]/60, 1) - inc) > tol):
-        res_fp.write(f"inc,{lat},{lon},{alt},{dyear},{res_map['dinc']},{inc}\n")
+    results_str = (",").join(inputs)
 
 
+    if os.path.isfile(file_path):
+        with open(file_path, "a") as f:
+            f.write(f"{results_str},{true_v},{pred_v} \n")
+    else:
+        with open(file_path, "w") as f:
+            f.write("date,lat,lon,alt,predict,true\n")
+            f.write(f"{results_str},{true_v},{pred_v} \n")
 
 
+def check_base_results(res_map, lat, lon, alt, dyear, dec, inc, h, x, y, z, f, tol, folder_path):
+
+    inputs = [str(dyear), str(lat), str(lon), str(alt)]
+    if (fabs(res_map["x"] - x) > tol):
+        filename = f"{folder_path}/x.csv"
+        write_diff_results(filename, inputs, res_map['x'], x)
+    if (fabs(res_map["y"] - y) > tol):
+        filename = f"{folder_path}/y.csv"
+        write_diff_results(filename, inputs, res_map['y'], y)
+    if (fabs(res_map["z"] - z) > tol):
+        filename = f"{folder_path}/z.csv"
+        write_diff_results(filename, inputs, res_map['z'], z)
+    if (fabs(res_map["h"] - h) > tol):
+        filename = f"{folder_path}/h.csv"
+        write_diff_results(filename, inputs, res_map['h'], h)
+    if (fabs(res_map["f"] - f) > tol):
+        filename = f"{folder_path}/f.csv"
+        write_diff_results(filename, inputs, res_map['f'], f)
+    if (fabs(res_map["dec"] - dec) > tol):
+        filename = f"{folder_path}/dec.csv"
+        write_diff_results(filename, inputs, res_map['dec'], dec)
+    if (fabs(res_map["inc"] - inc) > tol):
+        filename = f"{folder_path}/inc.csv"
+        write_diff_results(filename, inputs, res_map['inc'], inc)
 
 
-def refer_testValues(testval_filename, res_filename):
+def check_sv_results(res_map, lat, lon, alt, dyear, dec, inc, h, x, y, z, f, tol, folder_path):
 
+    inputs = [str(dyear), str(lat), str(lon), str(alt)]
+    if (fabs(res_map["dx"] - x) > tol):
+        filename = f"{folder_path}/x.csv"
+        write_diff_results(filename, inputs, res_map['dx'], x)
+    if (fabs(res_map["dy"] - y) > tol):
+        filename = f"{folder_path}/y.csv"
+        write_diff_results(filename, inputs, res_map['dy'], y)
+    if (fabs(res_map["dz"] - z) > tol):
+        filename = f"{folder_path}/z.csv"
+        write_diff_results(filename, inputs, res_map['dz'], z)
+    if (fabs(res_map["dh"] - h) > tol):
+        filename = f"{folder_path}/h.csv"
+        write_diff_results(filename, inputs, res_map['dh'], h)
+    if (fabs(res_map["df"] - f) > tol):
+        filename = f"{folder_path}/f.csv"
+        write_diff_results(filename, inputs, res_map['df'], f)
+    if (fabs(res_map["ddec"] / 60 - res_map["dec"]) > tol):
+        filename = f"{folder_path}/dec.csv"
+        write_diff_results(filename, inputs, res_map['ddec'] / 60, dec)
+    if (fabs(res_map["dinc"] / 60 - res_map["inc"]) > tol):
+        filename = f"{folder_path}/inc.csv"
+        write_diff_results(filename, inputs, res_map['dinc'] / 60, inc)
+
+
+def refer_testValues(testval_filename, res_folder, reserr_folder):
     wmm_model = wmm_calc()
 
     tol = 1e-6
-
-
-    res_fp = open(res_filename, "w")
-    resd_filename = "delta_"+res_filename
-    resd_fp = open(resd_filename, "w")
-
 
     with open(testval_filename, "r") as fp:
 
@@ -64,29 +85,38 @@ def refer_testValues(testval_filename, res_filename):
             else:
                 for i in range(len(vals)):
                     vals[i] = float(vals[i])
-                dyear, alt, lat, lon = vals[0], vals[1], vals[2],vals[3]
+                dyear, alt, lat, lon = vals[0], vals[1], vals[2], vals[3]
                 dec, inc, h, x, y, z, f = vals[4], vals[5], vals[6], vals[7], vals[8], vals[9], vals[10]
                 ddec, dinc, dh, dx, dy, dz, df = vals[11], vals[12], vals[13], vals[14], vals[15], vals[16], vals[17]
 
-                wmm_model.setup_time(dyear = dyear)
-                wmm_model.setup_env(lat, lon, alt, msl=False)
+                wmm_model.setup_time(dyear=float(dyear))
+                wmm_model.setup_env(float(lat), float(lon), float(alt), msl=False)
 
                 mag_map = wmm_model.get_all()
 
+                check_base_results(mag_map, lat, lon, alt, dyear, dec, inc, h, x, y, z, f, tol, res_folder)
+                check_sv_results(mag_map, lat, lon, alt, dyear, ddec, dinc, dh, dx, dy, dz, df, tol, reserr_folder)
 
 
-                check_base_results(mag_map, lat, lon, alt, dyear, dec, inc, h, x, y, z, f, tol, res_fp)
-                check_sv_results(mag_map, lat, lon, alt, dyear, ddec, dinc, dh, dx, dy, dz, df, tol, resd_fp)
 
-    res_fp.close()
-    resd_fp.close()
 
 def main():
-    testval_filename = "WMM2020_TEST_VALUES.txt"
-    res_filename = "res.csv"
-    refer_testValues(testval_filename, res_filename)
+    testval_filename = "WMM2025_FINAL_TEST_VALUES_HIGHPREC.txt"
+    res_folder="results"
+    reserr_folder="results_err"
 
-if __name__=="__main__":
+    if os.path.isdir(res_folder):
+        shutil.rmtree(res_folder)
+
+    os.mkdir(res_folder)
+
+    if os.path.isdir(reserr_folder):
+        shutil.rmtree(reserr_folder)
+
+    os.mkdir(reserr_folder)
+
+    refer_testValues(testval_filename, res_folder, reserr_folder)
+
+
+if __name__ == "__main__":
     main()
-
-
